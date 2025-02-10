ab669e527436c5c0a15e523c91eb6e2d0acd49c4
onyx <onyxceo1@gmail.com>
2025-02-10 21:33:37 +0300

    i think conflict is resolved

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 const welcome = document.getElementById("welcome");
 welcome.innerHTML = "Hello World From JS";
 
-=======
->>>>>>> 9bf5725a35b5953da72c542c081e8fbc5f413190
 console.log("Hello World");
 alert("Hello World");
